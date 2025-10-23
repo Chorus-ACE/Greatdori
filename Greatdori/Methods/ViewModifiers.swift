e7ab5a7c97961b8c2a56eefe2376e8e3e1f76cd0
WindowsMEMZ <memz1@darock.top>
2025-10-23 23:49:43 +0800

    Fix compilation problem (subject)

--- Greatdori/Methods/ViewModifiers.swift
+++ Greatdori/Methods/ViewModifiers.swift
@@ -242,6 +242,23 @@ extension View {
         }
 #endif
     }
+    func window<Content: View, Item: Identifiable>(
+        item: Binding<Item?>,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping (Item) -> Content
+    ) -> some View {
+#if os(macOS)
+        modifier(_AnyWindowModifier(isPresented: .init { item.wrappedValue != nil } set: { !$0 ? (item.wrappedValue = nil) : () }, onDismiss: onDismiss) {
+            item.wrappedValue != nil ? content(item.wrappedValue!) : nil
+        })
+#else
+        sheet(item: item, onDismiss: onDismiss) { item in
+            NavigationStack {
+                content(item)
+            }
+        }
+#endif
+    }
 }
 private struct _AnyWindowModifier<V: View>: ViewModifier {
     var isPresented: Binding<Bool>
@@ -381,12 +398,10 @@ private struct _ImageUpscaleView<V: View, Result: View>: View {
 }
 
 extension View {
-    // MARK: scrollDisablesMultilingualTextPopover
     func scrollDisablesMultilingualTextPopover(_ isEnabled: Bool = true) -> some View {
         ModifiedContent(content: self, modifier: ScrollDisableMultilingualTextPopoverModifier(isEnabled: isEnabled))
     }
 }
-
 private struct ScrollDisableMultilingualTextPopoverModifier: ViewModifier {
     var isEnabled: Bool
     @State private var disablesPopover = false
@@ -402,8 +417,6 @@ private struct ScrollDisableMultilingualTextPopoverModifier: ViewModifier {
         }
     }
 }
-
-// MARK: EnvironmentValues
 extension EnvironmentValues {
     @Entry var _multilingualTextDisablePopover: Bool = false
 }
