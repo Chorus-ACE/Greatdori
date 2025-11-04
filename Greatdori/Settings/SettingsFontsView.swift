5edcefdb4546b6802f46345049a89508ef96868a
ThreeManager785 <leimeichun50@gmail.com>
2025-11-04 21:12:41 +1000

    Intergrated Noto Sans Family

--- Greatdori/Settings/SettingsFontsView.swift
+++ Greatdori/Settings/SettingsFontsView.swift
@@ -19,6 +19,7 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 let fontManagerSampleText: [DoriLocale: String] = [.jp: "あなたの輝きが道を照らす", .en: "Your Spark Will Light the Way", .tw: "你的光芒照耀漫漫長路", .cn: "你的光芒会照亮前行之路", .kr: "당신의 반짝임이 길을 밝힌다"]
+let storyViewerDefaultFont: [DoriLocale: String] = [.jp: "Noto Sans JP", .en: "Noto Sans JP", .tw: "Noto Sans TC", .cn: "Noto Sans SC", .kr: ".AppleSystemUIFont"]
 
 struct SettingsFontsView: View {
     var body: some View {
@@ -97,7 +98,7 @@ struct SettingsFontsMain: View {
             }
             .onChange(of: storyViewerUpdateIndex, initial: true) {
                 for locale in DoriLocale.allCases {
-                    storyViewerFonts.updateValue(UserDefaults.standard.string(forKey: "StoryViewerFont\(locale.rawValue.uppercased())") ?? ".AppleSystemUIFont", forKey: locale)
+                    storyViewerFonts.updateValue(UserDefaults.standard.string(forKey: "StoryViewerFont\(locale.rawValue.uppercased())") ?? storyViewerDefaultFont[locale] ?? ".AppleSystemUIFont", forKey: locale)
                 }
             }
         }
@@ -360,7 +361,7 @@ struct SettingsFontsDetail: View {
                                         Text(fileName)
                                             .font(.custom(fontName, size: 10))
                                     } else {
-                                        Text("Settings.fonts.info.title.system-font")
+                                        Text(fontName.hasPrefix("Noto Sans") || fontName.hasPrefix("NotoSans") ? "Settings.fonts.info.title.built-in-font" : "Settings.fonts.info.title.system-font")
                                             .font(.custom(fontName, size: 10))
                                     }
                                 } else {
@@ -505,6 +506,19 @@ struct SettingsFontsDetail: View {
                             })
                         }
                     }
+                    
+                    if fontName.hasPrefix("Noto Sans") || fontName.hasPrefix("NotoSans") {
+                        Section {
+                            ListItem(allowValueLeading: true, title: {
+                                Text("Settings.fonts.info.license")
+                            }, value: {
+                                Text(oflLicense)
+                                    .multilineTextAlignment(.leading)
+                                    .fontDesign(.monospaced)
+                                    .font(.caption)
+                            })
+                        }
+                    }
                 }
                 .formStyle(.grouped)
                 .toolbar {
@@ -609,7 +623,7 @@ struct SettingsFontsPicker: View {
             addedSystemFonts = UserDefaults.standard.stringArray(forKey: "addedSystemFonts") ?? []
         }
         .onChange(of: updateIndex, initial: true) {
-            selectedFont = UserDefaults.standard.string(forKey: "StoryViewerFont\(locale.rawValue.uppercased())") ?? ".AppleSystemUIFont"
+            selectedFont = UserDefaults.standard.string(forKey: "StoryViewerFont\(locale.rawValue.uppercased())") ?? storyViewerDefaultFont[locale] ?? ".AppleSystemUIFont"
         }
         .onDisappear {
             externalUpdateIndex += 1
@@ -655,7 +669,7 @@ struct SettingsFontsPicker: View {
 final class FontManager: ObservableObject {
     static let shared = FontManager()
     static let allAcceptableSuffix: Set<String> = ["ttf", "otf", "ttc", "otc"]
-    static let builtInFonts = [".AppleSystemUIFont"]
+    static let builtInFonts = [".AppleSystemUIFont", "Noto Sans JP", "Noto Sans SC", "Noto Sans TC"]
     
     
     @Published var loadedFonts: [CachedFont] = []
@@ -721,8 +735,8 @@ final class FontManager: ObservableObject {
     
     func removeFontFromPreferences(_ fontName: String) {
         for locale in DoriLocale.allCases {
-            if (UserDefaults.standard.string(forKey: "StoryViewerFont\(locale.rawValue.uppercased())") ?? ".AppleSystemUIFont") == fontName {
-                UserDefaults.standard.set(".AppleSystemUIFont", forKey: "StoryViewerFont\(locale.rawValue.uppercased())")
+            if (UserDefaults.standard.string(forKey: "StoryViewerFont\(locale.rawValue.uppercased())") ?? "") == fontName {
+                UserDefaults.standard.set(storyViewerDefaultFont[locale] ?? ".AppleSystemUIFont", forKey: "StoryViewerFont\(locale.rawValue.uppercased())")
             }
         }
     }
@@ -891,3 +905,100 @@ func localizedLanguageName(for identifier: String, displayLocale: Locale = .curr
 extension Font.Weight {
     static var allCases: [Font.Weight] { [.thin, .ultraLight, .light, .regular, .medium, .semibold, .bold, .heavy, .black] }
 }
+
+
+let oflLicense = """
+Copyright 2014-2021 Adobe (http://www.adobe.com/), with Reserved Font Name 'Source'
+
+This Font Software is licensed under the SIL Open Font License, Version 1.1.
+This license is copied below, and is also available with a FAQ at:
+https://openfontlicense.org
+
+
+-----------------------------------------------------------
+SIL OPEN FONT LICENSE Version 1.1 - 26 February 2007
+-----------------------------------------------------------
+
+PREAMBLE
+The goals of the Open Font License (OFL) are to stimulate worldwide
+development of collaborative font projects, to support the font creation
+efforts of academic and linguistic communities, and to provide a free and
+open framework in which fonts may be shared and improved in partnership
+with others.
+
+The OFL allows the licensed fonts to be used, studied, modified and
+redistributed freely as long as they are not sold by themselves. The
+fonts, including any derivative works, can be bundled, embedded, 
+redistributed and/or sold with any software provided that any reserved
+names are not used by derivative works. The fonts and derivatives,
+however, cannot be released under any other type of license. The
+requirement for fonts to remain under this license does not apply
+to any document created using the fonts or their derivatives.
+
+DEFINITIONS
+"Font Software" refers to the set of files released by the Copyright
+Holder(s) under this license and clearly marked as such. This may
+include source files, build scripts and documentation.
+
+"Reserved Font Name" refers to any names specified as such after the
+copyright statement(s).
+
+"Original Version" refers to the collection of Font Software components as
+distributed by the Copyright Holder(s).
+
+"Modified Version" refers to any derivative made by adding to, deleting,
+or substituting -- in part or in whole -- any of the components of the
+Original Version, by changing formats or by porting the Font Software to a
+new environment.
+
+"Author" refers to any designer, engineer, programmer, technical
+writer or other person who contributed to the Font Software.
+
+PERMISSION & CONDITIONS
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of the Font Software, to use, study, copy, merge, embed, modify,
+redistribute, and sell modified and unmodified copies of the Font
+Software, subject to the following conditions:
+
+1) Neither the Font Software nor any of its individual components,
+in Original or Modified Versions, may be sold by itself.
+
+2) Original or Modified Versions of the Font Software may be bundled,
+redistributed and/or sold with any software, provided that each copy
+contains the above copyright notice and this license. These can be
+included either as stand-alone text files, human-readable headers or
+in the appropriate machine-readable metadata fields within text or
+binary files as long as those fields can be easily viewed by the user.
+
+3) No Modified Version of the Font Software may use the Reserved Font
+Name(s) unless explicit written permission is granted by the corresponding
+Copyright Holder. This restriction only applies to the primary font name as
+presented to the users.
+
+4) The name(s) of the Copyright Holder(s) or the Author(s) of the Font
+Software shall not be used to promote, endorse or advertise any
+Modified Version, except to acknowledge the contribution(s) of the
+Copyright Holder(s) and the Author(s) or with their explicit written
+permission.
+
+5) The Font Software, modified or unmodified, in part or in whole,
+must be distributed entirely under this license, and must not be
+distributed under any other license. The requirement for fonts to
+remain under this license does not apply to any document created
+using the Font Software.
+
+TERMINATION
+This license becomes null and void if any of the above conditions are
+not met.
+
+DISCLAIMER
+THE FONT SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+OF COPYRIGHT, PATENT, TRADEMARK, OR OTHER RIGHT. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+INCLUDING ANY GENERAL, SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL
+DAMAGES, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF THE USE OR INABILITY TO USE THE FONT SOFTWARE OR FROM
+OTHER DEALINGS IN THE FONT SOFTWARE.
+"""
