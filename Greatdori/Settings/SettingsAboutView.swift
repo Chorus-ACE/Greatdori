c410a78e0b380dcc943c13c833a2090c3ae4faf5
ThreeManager785 <leimeichun50@gmail.com>
2025-11-18 20:21:51 +1000

    Reorder

--- Greatdori/Settings/SettingsAboutView.swift
+++ Greatdori/Settings/SettingsAboutView.swift
@@ -140,15 +140,6 @@ struct SettingsAboutDetailIconView: View {
 
 struct SettingsAboutDetailListView: View {
     var body: some View {
-        SettingsDocumentButton(document: "T&C") {
-            HStack {
-                Text("Settings.about.user-agreement-license")
-                Spacer()
-                Image(systemName: "arrow.up.forward.circle")
-                    .foregroundStyle(.secondary)
-            }
-        }
-        .buttonStyle(.plain)
         Text(verbatim: "Licensed under Apache License 2.0")
         Link(destination: URL(string: "https://github.com/Greatdori")!, label: {
             HStack {
@@ -160,6 +151,15 @@ struct SettingsAboutDetailListView: View {
             .contentShape(Rectangle())
         })
         .foregroundStyle(.primary)
+        SettingsDocumentButton(document: "T&C") {
+            HStack {
+                Text("Settings.about.user-agreement-license")
+                Spacer()
+                Image(systemName: "arrow.up.forward.circle")
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .buttonStyle(.plain)
         NavigationLink(destination: {
             SettingsAboutAcknowledgementsView()
         }, label: {
