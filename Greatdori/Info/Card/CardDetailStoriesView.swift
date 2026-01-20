f4dbb6353f69859e574a7489dd0d32724d88f07c
WindowsMEMZ <memz1@darock.top>
2026-01-20 21:53:59 +0800

    Fix card story list spacing problem

--- Greatdori/Info/Card/CardDetailStoriesView.swift
+++ Greatdori/Info/Card/CardDetailStoriesView.swift
@@ -21,23 +21,25 @@ struct CardDetailStoriesView: View {
     var body: some View {
         if !information.card.episodes.isEmpty {
             Section {
-                if information.card.episodes[0].title.forLocale(locale) != nil {
-                    ForEach(Array(information.card.episodes.enumerated()), id: \.element.id) { index, story in
-                        StoryCardView(
-                            story: CustomStory(
-                                scenarioID: story.scenarioID,
-                                caption: story.episodeType.localizedString,
-                                title: story.title,
-                                synopsis: "",
-                                voiceAssetBundleName: nil
-                            ),
-                            type: .card,
-                            locale: locale,
-                            unsafeAssociatedID: information.card.resourceSetName
-                        )
+                VStack {
+                    if information.card.episodes[0].title.forLocale(locale) != nil {
+                        ForEach(Array(information.card.episodes.enumerated()), id: \.element.id) { index, story in
+                            StoryCardView(
+                                story: CustomStory(
+                                    scenarioID: story.scenarioID,
+                                    caption: story.episodeType.localizedString,
+                                    title: story.title,
+                                    synopsis: "",
+                                    voiceAssetBundleName: nil
+                                ),
+                                type: .card,
+                                locale: locale,
+                                unsafeAssociatedID: information.card.resourceSetName
+                            )
+                        }
+                    } else {
+                        DetailUnavailableView(title: "Details.unavailable.story", symbol: "books.vertical")
                     }
-                } else {
-                    DetailUnavailableView(title: "Details.unavailable.story", symbol: "books.vertical")
                 }
             } header: {
                 HStack {
