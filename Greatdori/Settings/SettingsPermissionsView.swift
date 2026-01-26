be31221cdc12520cf0776e47bfc7a83b7949bc76
WindowsMEMZ <memz1@darock.top>
2026-01-26 20:27:03 +0800

    Fix open system settings URL problem on iOS

--- Greatdori/Settings/SettingsPermissionsView.swift
+++ Greatdori/Settings/SettingsPermissionsView.swift
@@ -14,9 +14,7 @@
 
 import SwiftUI
 import DoriKit
-import EventKit
 import UserNotifications
-
 
 struct SettingsPermissionsView: View {
     @Environment(\.openURL) var openURL
@@ -24,64 +22,37 @@ struct SettingsPermissionsView: View {
     @State var permissionRejected: Bool = false
     var body: some View {
         Group {
-//#if os(iOS)
-//            Section(content: {
-//                SettingsPermissionsNotificationNews()
-//                SettingsPermissionsCalendarBirthdays()
-//            }, header: {
-//                Text("Settings.permissions")
-//            }, footer: {
-//                if permissionNotGiven || permissionRejected {
-//                    VStack(alignment: .leading) {
-//                        if permissionNotGiven {
-//                            Text("Settings.notifications.authorization-required.open-settings")
-//                                .multilineTextAlignment(.leading)
-//                                .onTapGesture {
-//                                    if let url = URL(string: UIApplication.openSettingsURLString) {
-//                                        openURL(url)
-//                                    }
-//                                }
-//                        }
-//                        if permissionRejected {
-//                            Text("Settings.notifications.access-denied")
-//                                .multilineTextAlignment(.leading)
-//                        }
-//                    }
-//                }
-//            })
-//#else
-            Group {
-                Section {
-                    SettingsPermissionsNotificationNews()
-                    SettingsPermissionsCalendarBirthdays()
-                }
-                Section(content: {
-                    Button(action: {
-                        openURL(.init(string: "x-apple.systempreferences:com.apple.preference.general")!)
-                    }, label: {
-                        Label("Settings.notifications.open-settings", systemImage: "arrow.up.right.square")
-                    })
-                    .buttonStyle(.plain)
-                }, footer: {
-                    if permissionNotGiven {
-                        Text("Settings.notifications.authorization-required")
-                    }
-                    if permissionRejected {
-                        Text("Settings.notifications.access-denied")
-                    }
-                })
+            Section {
+                SettingsPermissionsNotificationNews()
+                SettingsPermissionsCalendarBirthdays()
             }
-            .navigationTitle("Settings.permissions")
-//#endif
+            Section(content: {
+                Button(action: {
+                    #if !os(macOS)
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        openURL(url)
+                    }
+                    #else
+                    openURL(.init(string: "x-apple.systempreferences:com.apple.preference.notifications")!)
+                    #endif
+                }, label: {
+                    Label("Settings.notifications.open-settings", systemImage: "arrow.up.right.square")
+                })
+                .buttonStyle(.plain)
+            }, footer: {
+                if permissionNotGiven {
+                    Text("Settings.notifications.authorization-required")
+                }
+                if permissionRejected {
+                    Text("Settings.notifications.access-denied")
+                }
+            })
         }
+        .navigationTitle("Settings.permissions")
         .task {
             let notificationStatus = await UNUserNotificationCenter.current().notificationSettings()
             let notificationIsAuthorized = notificationStatus.authorizationStatus == .authorized
             let notificationIsRejected = notificationStatus.authorizationStatus == .denied
-            
-//            let calendarStatus = EKEventStore.authorizationStatus(for: .event)
-//            let calendarIsAuthorized = calendarStatus == .fullAccess
-//            let calendarIsRejected = calendarStatus == .denied
             
             permissionNotGiven = !notificationIsAuthorized
             permissionRejected = notificationIsRejected
@@ -100,97 +71,97 @@ struct SettingsPermissionsView: View {
         @State var shouldRespondToggleChange = true
         var body: some View {
             Group {
-                    Toggle(isOn: $isNewsNotificationEnabled, label: {
-                        VStack(alignment: .leading) {
-                            Text("Settings.notifications.news")
-                                .foregroundStyle((!notificationIsAuthorized && notificationIsRejected) ? .secondary : .primary)
-                                .onTapGesture {
-                                    if (!notificationIsAuthorized && notificationIsRejected) {
-#if os(iOS)
-                                        if let url = URL(string: UIApplication.openSettingsURLString) {
-                                            openURL(url)
-                                        }
-#else
-                                        openURL(.init(string: "x-apple.systempreferences:com.apple.preference.notifications")!)
-#endif
+                Toggle(isOn: $isNewsNotificationEnabled, label: {
+                    VStack(alignment: .leading) {
+                        Text("Settings.notifications.news")
+                            .foregroundStyle((!notificationIsAuthorized && notificationIsRejected) ? .secondary : .primary)
+                            .onTapGesture {
+                                if (!notificationIsAuthorized && notificationIsRejected) {
+                                    #if !os(macOS)
+                                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                                        openURL(url)
                                     }
+                                    #else
+                                    openURL(.init(string: "x-apple.systempreferences:com.apple.preference.notifications")!)
+                                    #endif
                                 }
+                            }
 //                            if notificationIsRejected {
 //                                Text("Settings.notifications.rejected")
 //                                    .foregroundStyle(.secondary)
 //                                    .font(.caption)
 //                            }
-                        }
-                    })
-                    .onChange(of: isNewsNotificationEnabled) {
-                        guard shouldRespondToggleChange else {
-                            shouldRespondToggleChange = true
-                            return
-                        }
-                        
-                        if isNewsNotificationEnabled {
-                            Task {
-                                if !notificationIsAuthorized && !notificationIsRejected {
-                                    do {
-                                        let granted = try await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge])
-                                        notificationIsAuthorized = granted
-                                        notificationIsRejected = true
-                                        if let token = UserDefaults.standard.data(forKey: "RemoteNotifDeviceToken") {
-                                            isNewsNotificationEnabled = true
-                                            Task {
-                                                if let id = await DoriNotification.registerRemoteNewsNotification(deviceToken: token) {
-                                                    UserDefaults.standard.set(id.uuidString, forKey: "NewsNotifID")
-                                                } else {
-                                                    isNewsNotificationEnabled = false
-                                                }
+                    }
+                })
+                .onChange(of: isNewsNotificationEnabled) {
+                    guard shouldRespondToggleChange else {
+                        shouldRespondToggleChange = true
+                        return
+                    }
+                    
+                    if isNewsNotificationEnabled {
+                        Task {
+                            if !notificationIsAuthorized && !notificationIsRejected {
+                                do {
+                                    let granted = try await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge])
+                                    notificationIsAuthorized = granted
+                                    notificationIsRejected = true
+                                    if let token = UserDefaults.standard.data(forKey: "RemoteNotifDeviceToken") {
+                                        isNewsNotificationEnabled = true
+                                        Task {
+                                            if let id = await DoriNotification.registerRemoteNewsNotification(deviceToken: token) {
+                                                UserDefaults.standard.set(id.uuidString, forKey: "NewsNotifID")
+                                            } else {
+                                                isNewsNotificationEnabled = false
                                             }
                                         }
-                                    } catch {
-                                        errorCode = -404
-                                        showErrorAlert = true
-                                        isNewsNotificationEnabled = false
                                     }
-                                }
-                                
-                                if let token = UserDefaults.standard.data(forKey: "RemoteNotifDeviceToken") {
-                                    Task {
-                                        if let id = await DoriNotification.registerRemoteNewsNotification(deviceToken: token) {
-                                            UserDefaults.standard.set(id.uuidString, forKey: "NewsNotifID")
-                                        } else {
-                                            shouldRespondToggleChange = false
-                                            isNewsNotificationEnabled = false
-                                            errorCode = -401
-                                            showErrorAlert = true
-                                        }
-                                    }
-                                } else {
-                                    shouldRespondToggleChange = false
-                                    isNewsNotificationEnabled = false
-                                    errorCode = -402
+                                } catch {
+                                    errorCode = -404
                                     showErrorAlert = true
+                                    isNewsNotificationEnabled = false
                                 }
                             }
                             
-                            
-                        } else {
-                            if let id = UserDefaults.standard.string(forKey: "NewsNotifID"),
-                               let uuid = UUID(uuidString: id) {
+                            if let token = UserDefaults.standard.data(forKey: "RemoteNotifDeviceToken") {
                                 Task {
-                                    let success = await DoriNotification.unregisterRemoteNewsNotification(id: uuid)
-                                    if !success {
+                                    if let id = await DoriNotification.registerRemoteNewsNotification(deviceToken: token) {
+                                        UserDefaults.standard.set(id.uuidString, forKey: "NewsNotifID")
+                                    } else {
                                         shouldRespondToggleChange = false
-                                        isNewsNotificationEnabled = true
-                                        errorCode = -403
+                                        isNewsNotificationEnabled = false
+                                        errorCode = -401
                                         showErrorAlert = true
                                     }
+                                }
+                            } else {
+                                shouldRespondToggleChange = false
+                                isNewsNotificationEnabled = false
+                                errorCode = -402
+                                showErrorAlert = true
+                            }
+                        }
+                        
+                        
+                    } else {
+                        if let id = UserDefaults.standard.string(forKey: "NewsNotifID"),
+                           let uuid = UUID(uuidString: id) {
+                            Task {
+                                let success = await DoriNotification.unregisterRemoteNewsNotification(id: uuid)
+                                if !success {
+                                    shouldRespondToggleChange = false
+                                    isNewsNotificationEnabled = true
+                                    errorCode = -403
+                                    showErrorAlert = true
                                 }
                             }
                         }
                     }
-                    .disabled(!notificationIsAuthorized && notificationIsRejected)
-                    .alert("Settings.notifications.news.error-alert.title", isPresented: $showErrorAlert, actions: {}, message: {
-                        Text("Settings.notifications.news.error-alert.message.\(errorCode)")
-                    })
+                }
+                .disabled(!notificationIsAuthorized && notificationIsRejected)
+                .alert("Settings.notifications.news.error-alert.title", isPresented: $showErrorAlert, actions: {}, message: {
+                    Text("Settings.notifications.news.error-alert.message.\(errorCode)")
+                })
             }
             .task {
                 let notificationStatus = await UNUserNotificationCenter.current().notificationSettings()
