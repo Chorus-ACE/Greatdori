ef707f6b942a3296500b06b87e5d794918e3066d
WindowsMEMZ <memz1@darock.top>
2026-02-14 15:28:02 +0800

    Fix gacha consumption table display in compact layout

--- Greatdori/Info/Gacha/GachaDetailConsumptionView.swift
+++ Greatdori/Info/Gacha/GachaDetailConsumptionView.swift
@@ -18,30 +18,56 @@ import SDWebImageSwiftUI
 
 struct GachaDetailConsumptionView: View {
     var information: ExtendedGacha
+    @Environment(\.horizontalSizeClass) private var sizeClass
     var body: some View {
         Section {
             CustomGroupBox {
                 VStack {
-                    Table(information.gacha.paymentMethods) {
-                        TableColumn("Gacha.costs.type") { paymentMethod in
-                            Text("Gacha.costs.type.plays.\(paymentMethod.count)")
-                        }
-                        TableColumn("Gacha.costs.costs") { paymentMethod in
-                            HStack {
-                                WebImage(url: paymentMethod.paymentMethod.iconImageURL)
-                                    .resizable()
-                                    .frame(width: 20, height: 20)
-                                Text(verbatim: "\(paymentMethod.paymentMethod.localizedString) x\(paymentMethod.count)")
+                    if sizeClass == .regular {
+                        Table(information.gacha.paymentMethods) {
+                            TableColumn("Gacha.costs.type") { paymentMethod in
+                                Text("Gacha.costs.type.plays.\(paymentMethod.count)")
+                            }
+                            TableColumn("Gacha.costs.costs") { paymentMethod in
+                                HStack {
+                                    WebImage(url: paymentMethod.paymentMethod.iconImageURL)
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                    Text(verbatim: "\(paymentMethod.paymentMethod.localizedString) x\(paymentMethod.count)")
+                                }
+                            }
+                            TableColumn("Gacha.costs.note") { paymentMethod in
+                                if paymentMethod.behavior != .normal {
+                                    Text(verbatim: "\(paymentMethod.behavior.localizedString)" + (paymentMethod.maxSpinLimit != nil ? ", " + String(localized: "Gacha.costs.note.maximum.\(paymentMethod.maxSpinLimit!)") : ""))
+                                }
                             }
                         }
-                        TableColumn("Gacha.costs.note") { paymentMethod in
-                            if paymentMethod.behavior != .normal {
-                                Text(verbatim: "\(paymentMethod.behavior.localizedString)" + (paymentMethod.maxSpinLimit != nil ? ", " + String(localized: "Gacha.costs.note.maximum.\(paymentMethod.maxSpinLimit!)") : ""))
+                        .scrollDisabled(true)
+                        .frame(height: 30*CGFloat(information.gacha.paymentMethods.count + 1))
+                    } else {
+                        Grid(alignment: .leading) {
+                            GridRow {
+                                Text("Gacha.costs.type")
+                                Text("Gacha.costs.costs")
+                                Text("Gacha.costs.note")
+                            }
+                            ForEach(Array(information.gacha.paymentMethods.enumerated()), id: \.element.id) { (index, paymentMethod) in
+                                Divider()
+                                GridRow {
+                                    Text("Gacha.costs.type.plays.\(paymentMethod.count)")
+                                    HStack {
+                                        WebImage(url: paymentMethod.paymentMethod.iconImageURL)
+                                            .resizable()
+                                            .frame(width: 20, height: 20)
+                                        Text(verbatim: "\(paymentMethod.paymentMethod.localizedString) x\(paymentMethod.count)")
+                                    }
+                                    if paymentMethod.behavior != .normal {
+                                        Text(verbatim: "\(paymentMethod.behavior.localizedString)" + (paymentMethod.maxSpinLimit != nil ? ", " + String(localized: "Gacha.costs.note.maximum.\(paymentMethod.maxSpinLimit!)") : ""))
+                                    }
+                                }
                             }
                         }
                     }
-                    .scrollDisabled(true)
-                    .frame(height: 30*CGFloat(information.gacha.paymentMethods.count + 1))
                 }
             }
             .frame(maxWidth: infoContentMaxWidth)
